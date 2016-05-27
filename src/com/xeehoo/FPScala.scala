825497f6b8d30eab0aa5ac194d71783918c50ea3
wangzunhui <wangzunhui@neowave.cn>
2016-05-27 11:54:49 +0800

    add curry fucntion

--- src/com/xeehoo/FPScala.scala
+++ src/com/xeehoo/FPScala.scala
@@ -40,11 +40,18 @@ object FPScala {
     loop(0)
   }
 
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+    b => f(a, b)
+
   def curry[A, B, C](f: (A, B) => C) : A => (B => C) = {
-    
+    (a : A) => (b : B) => f(a, b)
   }
 
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
 
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 
   def main(args: Array[String]) = {
     println("hello scala - " + fib(6))
